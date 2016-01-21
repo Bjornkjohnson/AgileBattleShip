7a32d4ce307e3e00de121422a2247f3c706fa512
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-01-20 19:18:01 -0500

    Change BoardPrint to single for loop
    Change printer to use single for loop to make it easier to find matching
    location.

--- src/com/bjorn/Game.java
+++ src/com/bjorn/Game.java
@@ -1,6 +1,7 @@
 package com.bjorn;
 
 public class Game {
+
     public void printBoard(Board newBoard) {
         System.out.print("  ");
         String topNumber = " %-2d ";
@@ -8,15 +9,19 @@ public class Game {
             System.out.format(topNumber, i);
         }
         System.out.print("\n");
-        for (int i = 0; i < 10; i++) {
-            System.out.print(i);
-            System.out.print("|_");
-            for (int j = 1; j < 10; j++){
+        for (int i = 0; i < 100; i++) {
+            if (i%10 == 0) {
+                System.out.print(i/10);
+                System.out.print("|_");
+            }
+            if (i%10 == 9){
+                System.out.print("~");
+                System.out.print("_|\n");
+            } else  {
                 System.out.print("~");
                 System.out.print("_|_");
             }
-            System.out.print("~");
-            System.out.print("_|\n");
+
         }
 
     }

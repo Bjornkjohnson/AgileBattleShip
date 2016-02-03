c0cdd8b324e67353e887ebd2ea1c00deab767834
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-02-02 19:32:55 -0500

    Create board setup class
    Create board setup class that can randomly place ships on board.
    Can be expanded for other board setup needs.

---
+++ src/com/bjorn/BoardSetup.java
@@ -0,0 +1,27 @@
+package com.bjorn;
+
+import java.util.Random;
+
+public class BoardSetup {
+    public Board randomBoard(Ship fleet[], Board newBoard) {
+        for (int i = 0; i < fleet.length; i++) {
+            while (true) {
+                try {
+                    fleet[i] = new Ship(2, getRandomNumber(), 0, "S");
+                    newBoard.placeShip(fleet[i]);
+                    break;
+                } catch (RuntimeException e) {
+                }
+            }
+        }
+        return newBoard;
+    }
+
+    private int getRandomNumber() {
+        Random r = new Random();
+        int Low = 0;
+        int High = 99;
+        return r.nextInt(High-Low) + Low;
+    }
+
+}
